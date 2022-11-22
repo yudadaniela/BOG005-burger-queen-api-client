d14e1b13230b30a1177b98b503cce22e20d9bb52
Daniela Nieto <yudadaniela@hotmail.com>
2022-11-22 17:51:29 -0500

    finalizando getusers

--- burguer-queen/src/views/getUsers/getUsers.js
+++ burguer-queen/src/views/getUsers/getUsers.js
@@ -7,6 +7,7 @@ import Modal from '../../components/modal';
 
 function GetUser() {
   const [isOpenModal, setisOpenModal] = useState(false)
+  const [currentUsers, setcurrentUsers] = useState([])
 
   const openModal = () => {
     setisOpenModal(true)
@@ -16,26 +17,52 @@ function GetUser() {
   }
 
   const getUserHandle =() =>{
-    getToken()
-    getRole()
+    // getToken()
+    // getRole()
     getUsers(getToken()).then(res => res.json()).then( rtaJson => {
-        console.log(rtaJson);
+        console.log(rtaJson);    
     })
   }
+  
+
+   useEffect(() => {
+    getUsers(getToken())
+      .then((response) => response.json())
+      .then((data) => {
+        setcurrentUsers(data)
+
+        // console.log(setcurrentUsers(data))
+
+      });
+  }, []);
+  useEffect(() => console.log(currentUsers), [currentUsers])
+
+  // const setcurrentUsersHandle=()=>{
+  //   setcurrentUsers(getUserHandle())
+  // }
+  
+
 
   return (
 
     <div className="App">
       <section className="App-header">
-
+       
         <button onClick={openModal}>abrir modal</button>
         <Modal
           isOpen={isOpenModal}
           closeModal={closeModal}
           contenido= <CreateUsersView />
         />
-
       </section>
+      <div>
+             {currentUsers.map((user, i) => {
+        return (<li key={i}> {user.id}, {user.email}, {user.password}, {user.role} </li>)
+         
+         })} 
+      </div>
+    
+
     </div>
   );
 }
