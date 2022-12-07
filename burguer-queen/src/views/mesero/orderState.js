b5f55d4a4d1488a38b2a63fb395a8b82e0171c83
Daniela Nieto <yudadaniela@hotmail.com>
2022-12-07 12:57:06 -0500

    checkbook

--- burguer-queen/src/views/mesero/orderState.js
+++ burguer-queen/src/views/mesero/orderState.js
@@ -26,48 +26,59 @@ const OrderState = () => {
         path2={"/waiter/orderState"}
         title2={"Estado de Pedidos"}
       />
-    <div className="subHeader"> 
-      <h1 className="title">Estados de Pedido</h1>
-      <p> Listos Para servir <VscBell/> </p>
-      {/* Falta agregar selector para filtrar */}
-    </div>
-    
-    <div className="tableContainerMain"> 
-      <div className="tableContainer">
-        <table className="headerTable">
-          <thead key={1} className="dataTable">
-            <tr key={1} className="titleTable">
-              Código
-            </tr>
-            <tr key={2} className="titleTable">
-              Fecha
-            </tr>
-            <tr key={3} className="titleTable">
-              Cliente
-            </tr>
-            <tr key={4} className="titleTable">
-              Total
-            </tr>
-            <tr key={4} className="titleTable">
-              Estado
-            </tr>
-          </thead>
-        </table>
-
-        {currentOrders.map((order, i) => {
-          return (
-            <table key={i}>
-              <tbody key={i} className="dataTable">
-                <tr key={i}> {order.id} </tr>
-                <tr> {order.dataEntry} </tr>
-                <tr> {order.client} </tr>
-                <tr> {order.total} </tr>
-                <tr> {order.status} </tr>
-              </tbody>
-            </table>
-          );
-        })}
+      <div className="subHeader">
+        <h1 className="title">Estados de Pedido</h1>
+        <p>
+          {" "}
+          Listos Para servir <VscBell />{" "}
+        </p>
+        {/* Falta agregar selector para filtrar */}
       </div>
+
+      <div className="tableContainerMain">
+        <div className="tableContainer">
+          <table className="headerTable">
+            <thead key={1} className="dataTable">
+              <tr key={1} className="titleTable">
+                Código
+              </tr>
+              <tr key={2} className="titleTable">
+                Fecha
+              </tr>
+              <tr key={3} className="titleTable">
+                Cliente
+              </tr>
+              <tr key={4} className="titleTable">
+                Total
+              </tr>
+              <tr key={4} className="titleTable">
+                Estado
+              </tr>
+            </thead>
+          </table>
+
+          {currentOrders.map((order, i) => {
+            return (
+              <table key={i}>
+                <tbody key={i} className="dataTable">
+                  <tr key={i}> {order.id} </tr>
+                  <tr> {order.dataEntry} </tr>
+                  <tr> {order.client} </tr>
+                  <tr> {order.total} </tr>
+                  <tr>
+                    {" "}
+                    {order.status}{" "}
+                    {order.status === "Listo para servir" ? (
+                      <input type="checkbox" />
+                    ) : (
+                      ""
+                    )}{" "}
+                  </tr>
+                </tbody>
+              </table>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
