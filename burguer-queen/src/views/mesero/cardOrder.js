fd2d66b016b37007281fce626babf2e95b3059d9
Lina Alvarado <linamariaalvaradopallares@gmail.com>
2022-12-07 17:49:08 -0500

    Saving changes

--- burguer-queen/src/views/mesero/cardOrder.js
+++ burguer-queen/src/views/mesero/cardOrder.js
@@ -1,37 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { createOrder, getToken } from "../../functions/requests";
-import { AiOutlineDelete } from "react-icons/ai";
-import './mesero.css'
+import { AiOutlineDelete, AiOutlineMinus } from "react-icons/ai";
+import "./mesero.css";
+import { BiPlus} from "react-icons/bi";
 
 const CardOrder = ({ productsListOrder, setproductsListOrder }) => {
   const [qtyProduct, setQtyProduct] = useState(true);
-  const[qtyTotal, setqtyTotal]= useState(1)
+  const [qtyTotal, setqtyTotal] = useState(1);
   const [nameClient, setNameClient] = useState("");
 
   const qtyPlusHandle = (event) => {
-   
-    console.log('entre a suma ');
+    console.log("entre a suma ");
     const productSelected = productsListOrder.filter(
       (p) => p.product.id == event.currentTarget.dataset.p
     );
-   console.log(productSelected[0].qty,'lo que filtro');
-   console.log(productsListOrder, 'pa ve');
-   productSelected[0].qty += 1;
-   productSelected[0].price = parseInt(productSelected[0].product.price) + parseInt(productSelected[0].price) 
+    console.log(productSelected[0].qty, "lo que filtro");
+    console.log(productsListOrder, "pa ve");
+    productSelected[0].qty += 1;
+    productSelected[0].price =
+      parseInt(productSelected[0].product.price) +
+      parseInt(productSelected[0].price);
     setproductsListOrder([...productsListOrder]);
-     setQtyProduct(true);
+    setQtyProduct(true);
   };
   const qtyRestHandle = (event) => {
-   
-    console.log('entre a suma ');
+    console.log("entre a suma ");
     const productSelected = productsListOrder.filter(
       (p) => p.product.id == event.currentTarget.dataset.p
     );
     console.log(productSelected[0].qty, "es lo que filtró");
-    if(productSelected[0].qty>=1){
+    if (productSelected[0].qty >= 1) {
       productSelected[0].qty -= 1;
-      productSelected[0].price =  parseInt(productSelected[0].price) - parseInt(productSelected[0].product.price) 
-    }else{
+      productSelected[0].price =
+        parseInt(productSelected[0].price) -
+        parseInt(productSelected[0].product.price);
+    } else {
       productSelected[0].qty -= 0;
     }
     setproductsListOrder([...productsListOrder]);
@@ -72,48 +75,74 @@ const CardOrder = ({ productsListOrder, setproductsListOrder }) => {
     setproductsListOrder([...productsListOrder]);
   };
 
-
   return (
-    <div >
+    <div>
       <form className="containerFormOrder" onSubmit={createOrderHandle}>
-        <div className="containerOneOrder"> 
-        <p className="textOrderTitle"> Pedido </p>
-        <input
-          className="inputNameClient"
-          type="text"
-          placeholder=" Escribe nombre del Cliente"
-          name="nameClient"
-          value={nameClient}
-          onChange={nameClientHandle}
-          data-testid="nameClient"
-          required
-        ></input>
-        {/* <p>Nombre del Cliente:{nameClient} </p> */}
+        <div className="containerOneOrder">
+          <p className="textOrderTitle"> Pedido </p>
+          <input
+            className="inputNameClient"
+            type="text"
+            placeholder=" Escribe nombre del Cliente"
+            name="nameClient"
+            value={nameClient}
+            onChange={nameClientHandle}
+            data-testid="nameClient"
+            required
+          ></input>
+          {/* <p>Nombre del Cliente:{nameClient} </p> */}
         </div>
         <div>
           {productsListOrder.map((p, i) => {
             return (
               <div className="pedido" key={i}>
-                <div className="containerProductsOrder"> 
+                <div className="containerProductsOrder">
                   <p>{p.product.name}</p>
-                  <p>{p.qty}</p>
+                  <div className="qtyContainer">
+                    {" "}
+                    <div
+                      className="addQtyButton"
+                      onClick={qtyPlusHandle}
+                      data-p={p.product.id}
+                    >
+                      {" "}
+                      <BiPlus/>
+                      {" "}
+                    </div>
+                    <p>{p.qty}</p>
+                    <div
+                  className="restQtyButton"
+                  onClick={qtyRestHandle}
+                  data-p={p.product.id}
+                >
+                  {" "}
+                  <AiOutlineMinus/>
+                  {" "}
+                </div>
+                  </div>
+                
                   <p>{p.price} </p>
                   <div onClick={deleteProduct} data-p={i}>
-                  {" "}
-                  <AiOutlineDelete />{" "}
+                    {" "}
+                    <AiOutlineDelete />{" "}
+                  </div>
                 </div>
-                </div>
-                <div onClick={qtyPlusHandle} data-p={p.product.id}>
+                {/* <div
+                  className="addQtyButton"
+                  onClick={qtyPlusHandle}
+                  data-p={p.product.id}
+                >
                   {" "}
                   sumar{" "}
                 </div>
-                <div onClick={qtyRestHandle} data-p={p.product.id}>
+                <div
+                  className="restQtyButton"
+                  onClick={qtyRestHandle}
+                  data-p={p.product.id}
+                >
                   {" "}
                   restar{" "}
-                </div>
-          
-              
-             
+                </div> */}
               </div>
             );
           })}
